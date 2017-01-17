a8febdb4d054c2947fd2ffc3da98cd539a00d7b8
Priya Sadhu <sadhu.priya@gmail.com>
2017-01-17 19:15:06 +0530

    Adding New Project

---
+++ src/test/java/com/seleniumjenkins/WebPageTest.java
@@ -0,0 +1,21 @@
+package com.seleniumjenkins;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class WebPageTest {
+	
+	@Test
+	public void TitleTest()
+	{
+		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://google.com");
+		String title = driver.getTitle();
+		System.out.println("Title=" + title);
+		driver.close();
+	}
+
+}
